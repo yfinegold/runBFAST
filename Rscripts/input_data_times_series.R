f8fa4df63866110f21432a1bde908dda55abf62d
yfinegold <yelena.finegold@fao.org>
2018-02-01 09:56:54 +0000

    simplify and clean scripts
    Former-commit-id: c6c4b2066dcbdf38313419942534c7c418f72664

--- Rscripts/input_data_times_series.R
+++ Rscripts/input_data_times_series.R
@@ -16,7 +16,6 @@ packages <- function(x){
     require(x,character.only=TRUE)
   }
 }
-
 packages(raster)
 packages(rgdal)
 packages(bfastSpatial)
@@ -27,17 +26,16 @@ packages(ggplot2)
 set_fast_options() # Optional, but should give some speed up
 
 # set results directory
+output_directory <-paste0(data_dir,"results/")
 if(!dir.exists(output_directory)){dir.create(output_directory, recursive = T)}
 setwd(output_directory)
-## input file locations
-forestmask <- paste0(mask_dir,forestmask_file)
+
+dates <- unlist(read.csv(paste0(data_dir, '/1/dates.csv')))
+data_input <- paste0(data_dir, '/1/stack.vrt')
 
 ## name of raster stack with 0 as no data
-NDMIstack_mask <- paste0(strsplit(data_input,".tif"),"_masked.tif")
-# NDVIstack_mask <- paste0(strsplit(NDVIstack_input,".tif"),"_masked.tif")
+stack_outputfile <- paste0(strsplit(data_input,'.tif'),'0NA.tif')
 
-NDMIstack_outputfile <- paste0(strsplit(data_input,'.tif'),'0NA.tif')
-# NDVIstack_outputfile <- paste0(strsplit(NDVIstack_input,'.tif'),'0NA.tif')
 # crop mask to AOI 
 # parameters
 resamp <- "near" #near (default), bilinear, cubic, cubicspline, lanczos, average, mode,  max, min, med, Q1, Q3
@@ -52,10 +50,10 @@ r.map1.xres <- res(r.map1)[1]
 r.map1.yres <- res(r.map1)[2]
 # r.map1.maxval <- system(sprintf("oft-mm -um %s %s | grep 'Band 1 max = '",paste0(inputdir, map1), paste0(inputdir, map1)), intern = TRUE)
 r.map1.proj <- as.character(projection(r.map1))
-# (strsplit(r.map1.proj,'+ellps='))
-# r.map1.proj
-# +proj=longlat +datum=WGS84 +no_defs'
+
 if(mask_data==1){
+  ## input file locations
+  forestmask <- paste0(mask_dir,forestmask_file)
   r.map2 <- raster(forestmask)
   r.map2.xmin <- as.matrix(extent(r.map2))[1]
   r.map2.ymin <- as.matrix(extent(r.map2))[2]
@@ -65,7 +63,6 @@ if(mask_data==1){
   r.map2.yres <- res(r.map2)[2]
   # r.map2.maxval <- system(sprintf("oft-mm -um %s %s | grep 'Band 1 max = '",paste0(inputdir, map2), paste0(inputdir, map2)), intern = TRUE)
   r.map2.proj <- as.character(projection(r.map2))
-  
   
   # step 2
   # reproject if needed and warp
@@ -99,48 +96,21 @@ if(mask_data==1){
                    paste0(mask_dir, forestmask_file),
                    paste0(data_dir, 'tmp_warp_',forestmask_file)
     ))
-    
   }
-  
   # mask out non-forest
   system(sprintf("gdal_calc.py -A %s -B %s --A_band=1 --co COMPRESS=LZW --NoDataValue=0 --allBands=A --overwrite --outfile=%s --calc=\"%s\"",
                  data_input,
                  paste0(data_dir, 'tmp_warp_',forestmask_file),
-                 paste0(strsplit(NDMIstack_file,".tif"),"_masked.tif"),
+                 paste0(strsplit(stack_file,".tif"),"_masked.tif"),
                  "(A*B)"
   ))
   system(sprintf("gdal_translate -co COMPRESS=LZW  -a_nodata 0  %s %s",
-                 paste0(getwd(),'/',strsplit(NDMIstack_file,".tif"),"_masked.tif"),
-                 NDMIstack_outputfile
+                 paste0(getwd(),'/',strsplit(stack_file,".tif"),"_masked.tif"),
+                 stack_outputfile
   ))
-  if(NDMI_only==1){
-    system(sprintf("gdal_calc.py -A %s -B %s --A_band=1 --co COMPRESS=LZW --NoDataValue=0 --allBands=A --overwrite --outfile=%s --calc=\"%s\"",
-                   NDVIstack_input,
-                   paste0(data_dir, 'tmp_warp_',forestmask_file),
-                   paste0(strsplit(NDVIstack_file,".tif"),"_masked.tif"),
-                   "(A*B)"
-    ))
-    system(sprintf("gdal_translate -co COMPRESS=LZW -a_nodata 0 %s %s",
-                   paste0(getwd(),'/',strsplit(NDVIstack_file,".tif"),"_masked.tif"),
-                   NDVIstack_outputfile
-    ))
-  }
+  NDMIstack <- brick(stack_outputfile) 
+
 }else{
-  # system(sprintf("gdal_translate -co COMPRESS=LZW  -a_nodata 0  %s %s",
-  #                data_input,
-  #                NDMIstack_outputfile
-  # ))
-  if(NDMI_only==1){
-    # system(sprintf("gdal_translate -co COMPRESS=LZW -a_nodata 0 %s %s",
-    #                NDVIstack_input,
-    #                NDVIstack_outputfile
-    # ))
-  }
-}
-
-if(NDMI_only==1){
-  NDVIstack <- brick(NDVIstack_input)
-}
-
 ## read images as raster stack
 NDMIstack <- brick(data_input) 
+}
--- run_all_times_series.R
+++ run_all_times_series.R
@@ -12,49 +12,8 @@
 ####################################################################################
 
 ## input data for BFAST scripts 
-
 # set data directory
 data_dir <- '/home/finegold/downloads/ETH_kamashi/' # timeseries directory
-
-
-# set forest mask directory
-mask_dir <- "~/runBFAST/example/mask/"
-
-# Forest mask, 1 is forest and 0 is nonforest
-# forest mask file name
-forestmask_file <- 'sieved_LC_2010_forestmask.tif'
-
-
-# # basename for the raster stacks and CSV files
-# # if the file is called ndmi_time_series_stack_Kyanja.tif the basename is Kyanja
-# basename <- "Menagesha"
-# 
-# 
-# ####### Run the GEE script: https://code.earthengine.google.com/bb245767014bc48657d121e1a8747098
-# 
-# ####### Transfer data from Google Drive to SEPAL
-# ####### Example of authorization key : 4/QHH2DucZ-MI-GY0HnG6JyEfjMpfVvJsu6_TmHqbxBgQ
-# 
-# setwd(data_dir)
-# ## paste the long link into the browser, follow the instructions to connect to your google account
-# ## then copy the key and replace PASTE_THE_KEY_HERE with your authorization key
-# system(sprintf("echo %s | drive init",
-#                "4/-RHV07YV7jM90vTZR2KfG-tdzd7lGuGgvgrjaGcR9bU"))
-# 
-# # system(sprintf("echo %s | drive init"))
-# system(sprintf("drive list"))
-# 
-# data_input <- c(paste0(c('ndmi_time_series_stack_','ndvi_time_series_stack_'),basename,'.tif'),
-#                 paste0(c('ndmi_time_series_stack_','ndmi_time_series_stack_'),basename,'.csv')
-# )
-# 
-# for(data in data_input){
-#   system(sprintf("drive pull %s",
-#                  data))
-# }
-# 
-dates <- unlist(read.csv(paste0(data_dir, '/1/dates.csv')))
-data_input <- paste0(data_dir, '/1/stack.vrt')
 
 # beginning of historical period
 historical_year_beg <- 2008
@@ -63,18 +22,21 @@ monitoring_year_beg <- 2013
 # end of monitoring period
 monitoring_year_end <- 2017
 
-# do you want to use a forest mask?
+##############################################
+##       OPTIONAL SETTING             ########
+## do you want to use a forest mask?  ########
+##############################################
+
 # 1 = use a forest mask
 # 0 = do not use a forest mask
 mask_data <- 0
 
-# do you want to test only NDMI?
-# 1 = use NDMI and NDVI
-# 0 = use only NDMI
-NDMI_only <- 0
+# set forest mask directory
+mask_dir <- "~/runBFAST/example/mask/"
 
-# set results directory
-output_directory <-paste0(data_dir,"results/")
+# Forest mask, 1 is forest and 0 is nonforest
+# forest mask file name
+forestmask_file <- 'sieved_LC_2010_forestmask.tif'
 
 #################################
 # Run R scripts
